1fe62b6efeb4d555bf1b86eb156c70820f31ba5f
Samandar Odilboyev <samandarodilboyev@gmail.com>
2021-05-06 08:55:39 +0500

    localstorage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  } else{
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState('')
-  const [list, setList] = useState([])
+  const [list, setList] = useState(getLocalStorage)
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({show:false, msg: '', type:''});
@@ -33,7 +42,7 @@ function App() {
       const newItem =  {id: new Date().getTime().toString(), title: name};
       setList([...list, newItem])
       setName('');
-      showAlert(true, "success", "Todo added")
+      showAlert(true, "success", "Task added")
     }
   }
   
@@ -43,12 +52,12 @@ function App() {
   }
 
   const clearList = () => {
-    showAlert(true, 'danger', 'Todo deleted')
+    showAlert(true, 'danger', 'Tasks deleted')
     setList([])
   }
 
   const removeItem = (id) => {
-    showAlert(true, 'danger', 'item deleted')
+    showAlert(true, 'danger', 'Task deleted')
     setList(list.filter(item => item.id !== id))
   }
 
@@ -58,16 +67,21 @@ function App() {
     setEditID(id)
     setName(specificItem.title)
   }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list]);
+
   return <section className="section-center">
 
     <form className="grocery-form" onSubmit={handleSubmit}>
       {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-      <h3>Grocery Bud</h3>
+      <h3>Tasks list</h3>
       <div className="form-control">
         <input 
         type="text" 
         className="grocery" 
-        placeholder="ex. egg"  
+        placeholder="type a task..."  
         value={name}
         onChange={(e) => setName(e.target.value)}
         />
